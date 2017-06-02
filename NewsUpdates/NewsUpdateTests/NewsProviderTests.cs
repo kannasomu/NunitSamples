706fc0efcc31722e91eba0bac36b84ac2bc0be2a
Kannappan Somasundaram <Kannappan.Somasundaram@thetrainline.com>
2017-06-02 21:12:36 +0100

    Adding test + code for news updates

---
+++ NewsUpdates/NewsUpdateTests/NewsProviderTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NewsUpdates;
+using NUnit.Framework;
+using RestSharp;
+
+namespace NewsUpdateTests
+{
+    [TestFixture]
+    public class NewsProviderTests
+    {
+        private Mock<IRestClient> _restclientMock;
+        private NewsProvider _newsprovider; 
+
+        [SetUp]
+        public void SetUp()
+        {
+            _restclientMock = new Mock<IRestClient>();
+            _newsprovider = new NewsProvider(_restclientMock.Object);
+        }
+
+
+        [Test]
+        public void ShouldGetNewsItemOnATopicIfPresent()
+        {
+            
+            _restclientMock.Setup(x => x.Execute(It.IsAny<RestRequest>())).Returns(new RestResponse()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = "{\n \"articles\": [\n {\n \"author\": \"BBC News\",\n \"title\": \"Title manchester\",\n\"description\": \"Text\",\n\"publishedAt\": \"2017-06-02T14:52:59+00:00\"\n }\n]\n}"
+            });
+
+            var newsItem = _newsprovider.GetLatestNewItem("manchester");
+
+            Assert.That(newsItem.Date, Is.EqualTo("2017-06-02T14:52:59+00:00"));
+            Assert.That(newsItem.Text, Is.EqualTo("Text"));
+            Assert.That(newsItem.Title, Is.EqualTo("Title manchester"));
+            _restclientMock.Verify(x => x.Execute(It.IsAny<RestRequest>()), Times.Once);
+        }
+
+
+
+        [Test]
+        public void ShouldGetNullResponseIfTopicIsNotPresent()
+        {
+            _restclientMock.Setup(x => x.Execute(It.IsAny<RestRequest>())).Returns(new RestResponse()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = "{\n \"articles\": [\n {\n \"author\": \"BBC News\",\n \"title\": \"Title manchester\",\n\"description\": \"Text\",\n\"publishedAt\": \"2017-06-02T14:52:59+00:00\"\n }\n]\n}"
+            });
+
+            var newsItem = _newsprovider.GetLatestNewItem("election");
+
+            Assert.That(newsItem, Is.Null);
+        }
+
+        [Test]
+        public void ShouldGetExceptionIfApiThrowsException()
+        {
+            var restclientMock = new Mock<IRestClient>();
+            var newsprovider = new NewsProvider(restclientMock.Object);
+            restclientMock.Setup(x => x.Execute(It.IsAny<RestRequest>()))
+                .Throws(new Exception("Unable to connect to server"));
+
+            Assert.That(() => newsprovider.GetLatestNewItem(null),
+                Throws.Exception
+                    .TypeOf<Exception>()
+                    .With.Message
+                    .EqualTo("Unable to connect to server"));
+        }
+    }
+}
---
+++ NewsUpdates/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace NewsUpdates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var topicToSearchFor = args[0];
+            Console.WriteLine($"Latest news on {topicToSearchFor}");
+
+            var newsProvider = new NewsProvider(new RestClient(new Uri(@"https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=1233")));
+            var newsItem = newsProvider.GetLatestNewItem(topicToSearchFor);
+            
+            Console.WriteLine($"Title: {newsItem.Title}");
+            Console.WriteLine($"Date: {newsItem.Date}");
+            Console.WriteLine("Text");
+            Console.WriteLine(newsItem.Text);
+        }
+    }
+
+    public class NewsItem
+    {
+        public string Title { get; set; }
+        public string Date { get; set; }
+        public string Text { get; set; }
+    }
+    
+    public class NewsProvider
+    {
+        private readonly IRestClient _restClient;
+        public NewsProvider(IRestClient restClient)
+        {
+            _restClient = restClient;
+        }
+        public NewsItem GetLatestNewItem(string topicName)
+        {
+            var response = _restClient.Execute(new RestRequest());
+            var news = JsonConvert.DeserializeObject<News>(response.Content);
+
+            if (news.Articles.FirstOrDefault(x => x.Title.Contains(topicName)) != null)
+            {
+                return new NewsItem()
+                {
+                    Date = news.Articles[0].PublishedAt,
+                    Title = news.Articles[0].Title,
+                    Text = news.Articles[0].Description
+                };
+            }
+            return null;
+        }
+    }
+
+    public class Article
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string UrlToImage { get; set; }
+        public string PublishedAt { get; set; }
+    }
+
+    public class News
+    {
+        public string Status { get; set; }
+        public string Source { get; set; }
+        public string SortBy { get; set; }
+        public List<Article> Articles { get; set; }
+    }
+
+}
