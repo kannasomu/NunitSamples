b043dcacbb02dc95cfcf31a7790eade6c6e08267
Kannappan Somasundaram <Kannappan.Somasundaram@thetrainline.com>
2017-06-03 10:09:44 +0100

    Changing mocking to Nsubstitute

--- NewsUpdates/NewsUpdateTests/NewsProviderTests.cs
+++ NewsUpdates/NewsUpdateTests/NewsProviderTests.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using Moq;
 using NewsUpdates;
+using NSubstitute;
 using NUnit.Framework;
 using RestSharp;
 
@@ -14,22 +10,21 @@ namespace NewsUpdateTests
     [TestFixture]
     public class NewsProviderTests
     {
-        private Mock<IRestClient> _restclientMock;
+        private IRestClient _restclientMock;
         private NewsProvider _newsprovider; 
 
         [SetUp]
         public void SetUp()
         {
-            _restclientMock = new Mock<IRestClient>();
-            _newsprovider = new NewsProvider(_restclientMock.Object);
+             _restclientMock = Substitute.For<IRestClient>();
+            _newsprovider = new NewsProvider(_restclientMock);
         }
 
 
         [Test]
         public void ShouldGetNewsItemOnATopicIfPresent()
         {
-            
-            _restclientMock.Setup(x => x.Execute(It.IsAny<RestRequest>())).Returns(new RestResponse()
+            _restclientMock.Execute(Arg.Any<RestRequest>()).Returns(new RestResponse()
             {
                 StatusCode = HttpStatusCode.OK,
                 Content = "{\n \"articles\": [\n {\n \"author\": \"BBC News\",\n \"title\": \"Title manchester\",\n\"description\": \"Text\",\n\"publishedAt\": \"2017-06-02T14:52:59+00:00\"\n }\n]\n}"
@@ -40,15 +35,14 @@ namespace NewsUpdateTests
             Assert.That(newsItem.Date, Is.EqualTo("2017-06-02T14:52:59+00:00"));
             Assert.That(newsItem.Text, Is.EqualTo("Text"));
             Assert.That(newsItem.Title, Is.EqualTo("Title manchester"));
-            _restclientMock.Verify(x => x.Execute(It.IsAny<RestRequest>()), Times.Once);
+            _restclientMock.Received().Execute(Arg.Any<RestRequest>());
         }
-
 
 
         [Test]
         public void ShouldGetNullResponseIfTopicIsNotPresent()
         {
-            _restclientMock.Setup(x => x.Execute(It.IsAny<RestRequest>())).Returns(new RestResponse()
+            _restclientMock.Execute(Arg.Any<RestRequest>()).Returns(new RestResponse
             {
                 StatusCode = HttpStatusCode.OK,
                 Content = "{\n \"articles\": [\n {\n \"author\": \"BBC News\",\n \"title\": \"Title manchester\",\n\"description\": \"Text\",\n\"publishedAt\": \"2017-06-02T14:52:59+00:00\"\n }\n]\n}"
@@ -57,17 +51,16 @@ namespace NewsUpdateTests
             var newsItem = _newsprovider.GetLatestNewItem("election");
 
             Assert.That(newsItem, Is.Null);
+            _restclientMock.Received(1).Execute(Arg.Any<RestRequest>());
         }
 
         [Test]
         public void ShouldGetExceptionIfApiThrowsException()
         {
-            var restclientMock = new Mock<IRestClient>();
-            var newsprovider = new NewsProvider(restclientMock.Object);
-            restclientMock.Setup(x => x.Execute(It.IsAny<RestRequest>()))
-                .Throws(new Exception("Unable to connect to server"));
+            _restclientMock.When(x => x.Execute(Arg.Any<RestRequest>()))
+                .Do(x => { throw new Exception("Unable to connect to server"); });
 
-            Assert.That(() => newsprovider.GetLatestNewItem(null),
+            Assert.That(() => _newsprovider.GetLatestNewItem(null),
                 Throws.Exception
                     .TypeOf<Exception>()
                     .With.Message
